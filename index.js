dfbc510edf360480700faafb47d5baada2d95740
MERCYGATUNE <ariri691@gmail.com>
2024-03-24 13:52:38 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"] ;
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+ function destructivelyPrependCat(name) {  
+     cats.unshift('Bob');
+ }
+ function destructivelyRemoveLastCat() {
+     cats.pop();
+ }
+ function destructivelyRemoveFirstCat() {
+     cats.shift();
+ }
+ function appendCat(name) {
+     return [...cats , 'Broom'];
+ }
+ function prependCat(name) {
+     return ['Arnold',...cats];
+ }
+
+ const newCats = [...cats];
+
+ function removeLastCat() {
+    const newCats = [...cats];
+     newCats.pop();
+     return newCats;
+ }
+function removeFirstCat() {
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+
+}
+
